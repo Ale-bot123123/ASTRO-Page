3861fe0b28cb6f3f35eae315a803720ffa2aac87
Alejo Milani <alegena123@gmail.com>
2023-05-26 11:32:11 -0300

    Estructura basica del header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import HeaderFixed from "./components/HeaderFixed.js"
+import HeaderSection from "./components/HeaderSection.js"
+import SocialLinks from "./components/SocialLinks.js"
 
 function App() {
   return (
     <div className="App">
+      <HeaderFixed/>
+      <HeaderSection/>
+      <SocialLinks/>
     </div>
   );
 }
