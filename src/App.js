0fa1f71740ab970c29c8975ec2cfaa7eb97a0a39
Alejo Milani <alegena123@gmail.com>
2023-05-26 23:42:11 -0300

    Base de la seccion de Competencia terminada

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import HeaderFixed from "./components/HeaderFixed.js"
 import HeaderSection from "./components/HeaderSection.js"
 import SocialLinks from "./components/SocialLinks.js"
+import CompetenciaSection from "./components/CompetenciaSection.js"
 
 function App() {
   return (
     <div className="App">
       <HeaderFixed/>
-      <HeaderSection/>
+      <main>
+        <HeaderSection/>
+        <CompetenciaSection/>
+      </main>
       <SocialLinks/>
     </div>
   );
---
+++ src/components/CompetenciaSection.js
@@ -0,0 +1,17 @@
+import './styles/CompetenciaSection.css';
+import cansatArg from "../assets/CansatArg.png"
+
+function CompetenciaSection() {
+    return (
+        <section className="CompetenciaSection">
+            <div className="CompetenciaInfoContainer">
+                <h2 className="CompetenciaTitle">COMPETENCIA</h2>
+                <p className="CompetenciaInfo">CANSAT es una competencia que propone a estudiantes construir una carga útil, cuyo tamaño no supere el de una lata de gaseosa, y lanzarla en un cohete hasta una altitud de aproximadamente un kilómetro. De allí, el término CAN (lata) y SAT (satélite), por sus siglas en inglés.</p>
+                <p className="CompetenciaInfo">El objetivo de la competencia es contribuir al desarrollo de habilidades tecnológicas y a la promoción de una cultura científica e innovadora, ampliando la base de futuros profesionales y fortaleciendo los medios que aseguren la sostenibilidad del sector espacial en la Argentina, con un criterio federal.</p>
+            </div>
+            <img src={cansatArg} className='CansatArg' alt='Cansat'></img>
+        </section>
+    );
+  }
+  
+  export default CompetenciaSection;
